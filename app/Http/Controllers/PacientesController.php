2dbe341504a27b4ce58b761b2669ef8a512ae06c
gamalavi <jonatan.lavarreda54@gmail.com>
2022-10-25 20:49:50 -0600

    fixed

--- app/Http/Controllers/PacientesController.php
+++ app/Http/Controllers/PacientesController.php
@@ -201,102 +201,85 @@ class PacientesController extends Controller
     {
         $depto = DB::select('select ID_DEPARTAMENTO, DEPARTAMENTO from tb_departamento ');
         $parentesco = DB::select('select id_parentesco , tipo_parentesco from tb_parentesco');
-        $paciente = DB::select('select 
-                                    p.no_expediente as noExpediente,
-                                    id_paciente as id,
-                                    no_expediente as expediente,
-                                    nombre_1,
-                                    nombre_2,
-                                    nombre_3,
-                                    apellido_1,
-                                    apellido_2,
-                                    apellido_de_casada,
-                                    estado_civil,
-                                    p.iddatos_dpi as id_datos_dpi,
-                                    p.acceso_al_igss,
-                                    p.nacionalidad,
-                                    p.religion,
-                                    p.edad,
-                                    p.genero,
-                                    p.fecha_nacimiento,
-                                    p.id_estado_paciente_activo as paciente_activo,
-                                    p.id_estado_paciente_inactivo as paciente_inactivo,
-                                    p.religion,
-                                    p.direccion,
-                                    p.zona,
-                                    p.colonia_barrio_aldea,
-                                    p.referencia_vivienda as referencia_vivienda,
-                                    p.telefono_casa,
-                                    p.celular_1,
-                                    p.Celular_2 ,
-                                    p.id_familiar_responsable as id_familiar_responsable,
-                                    mactual.id_municipio as id_municipio_actual,
-                                    mactual.municipio as municipio_actual,
-                                    dactual.id_departamento as id_departamento_actual,
-                                    dactual.departamento as departamento_actual,
-                                    dd.iddatos_dpi as id_datos_dpi,
-                                    dd.dpi as dpi,
-                                    dd.estado_dpi as estado_dpi,
-                                    dd.fecha_vencimiento as fecha_vencimiento_dpi,
-                                    mdpi.id_municipio as id_municipio_dpi,
-                                    mdpi.municipio as municipio_dpi,
-                                    dDpi.id_departamento as id_departamento_dpi,
-                                    dDpi.departamento as departamento_dpi
-                                from tb_paciente p 
-                                inner join tb_municipio mactual on mactual.ID_MUNICIPIO = p.ID_Municipio 
-                                inner join tb_departamento dactual on dactual.ID_DEPARTAMENTO = mactual.ID_DEPARTAMENTO
-                                inner join tb_datos_dpi dd on dd.idDatos_DPI = p.idDatos_DPI
-                                inner join tb_municipio mdpi on mdpi.ID_MUNICIPIO = dd.ID_Municipio 
-                                inner join tb_departamento dDpi on dDpi.ID_DEPARTAMENTO = mdpi.ID_DEPARTAMENTO
-                                where id_Paciente = ' . $id_Paciente);
+        $paciente = DB::select("select 
+        p.no_expediente as noExpediente,
+        id_paciente as id,
+        no_expediente as expediente,
+        nombre_1,
+        nombre_2,
+        nombre_3,
+        apellido_1,
+        apellido_2,
+        apellido_de_casada,
+        estado_civil,
+        p.iddatos_dpi as id_datos_dpi,
+        p.acceso_al_igss,
+        p.nacionalidad,
+        p.edad,
+        p.genero,
+        p.fecha_nacimiento,
+        p.id_estado_paciente as paciente_activo, 
+        p.religion,
+        p.direccion,
+        p.zona,
+        p.colonia_barrio_aldea,
+        p.referencia_vivienda as referencia_vivienda,
+        p.telefono_casa,
+        p.celular_1,
+        IF(Celular_2 = 0, '', p.Celular_2 ) as Celular_2,
+        p.id_familiar_responsable as id_familiar_responsable,
+        mactual.id_municipio as id_municipio_actual,
+        mactual.municipio as municipio_actual,
+        dactual.id_departamento as id_departamento_actual,
+        dactual.departamento as departamento_actual,
+        dd.iddatos_dpi as id_datos_dpi,
+        IF(dd.dpi = 0, '', dd.dpi ) as dpi,
+        dd.estado_dpi as estado_dpi,
+        dd.fecha_vencimiento as fecha_vencimiento_dpi,
+        mdpi.id_municipio as id_municipio_dpi,
+        mdpi.municipio as municipio_dpi,
+        ddpi.id_departamento as id_departamento_dpi,
+        ddpi.departamento as departamento_dpi
+    from tb_paciente p 
+    inner join tb_municipio mactual on mactual.ID_MUNICIPIO = p.ID_Municipio 
+    inner join tb_departamento dactual on dactual.ID_DEPARTAMENTO = mactual.ID_DEPARTAMENTO
+    inner join tb_datos_dpi dd on dd.idDatos_DPI = p.idDatos_DPI
+    inner join tb_municipio mdpi on mdpi.ID_MUNICIPIO = dd.ID_Municipio 
+    inner join tb_departamento ddpi on ddpi.ID_DEPARTAMENTO = mdpi.ID_DEPARTAMENTO
+    where id_Paciente = " . $id_Paciente);
 
-        $generalPacientes = DB::SELECT('select 
-                                            id_paciente as id,
-                                            no_expediente as expediente,
-                                            nombre_1,
-                                            nombre_2,
-                                            nombre_3,
-                                            apellido_1,
-                                            apellido_2,
-                                            apellido_de_casada,
-                                            estado_civil,
-                                            p.iddatos_dpi as id_datos_dpi,
-                                            p.acceso_al_igss,
-                                            p.nacionalidad,
-                                            p.religion,
-                                            p.edad,
-                                            p.genero,
-                                            p.fecha_nacimiento,
-                                            p.id_estado_paciente_activo as paciente_activo,
-                                            p.id_estado_paciente_inactivo as paciente_inactivo,
-                                            p.religion,
-                                            p.direccion,
-                                            p.zona,
-                                            p.colonia_barrio_aldea,
-                                            p.referencia_vivienda as referencia_vivienda,
-                                            p.telefono_casa,
-                                            p.celular_1,
-                                            p.Celular_2 ,
-                                            p.id_familiar_responsable as id_familiar_responsable,
-                                            mActual.ID_Municipio as id_municipio_actual,
-                                            mActual.municipio as municipio_actual,
-                                            dActual.id_departamento as id_departamento_actual,
-                                            dActual.departamento as departamento_actual,
-                                            dd.iddatos_dpi as id_datos_dpi,
-                                            dd.dpi as dpi,
-                                            dd.estado_dpi as estado_dpi,
-                                            dd.fecha_vencimiento as fecha_vencimiento_dpi,
-                                            mDpi.id_municipio as id_municipio_dpi,
-                                            mDpi.municipio as municipio_dpi,
-                                            dDpi.id_departamento as id_departamento_dpi,
-                                            dDpi.departamento as departamento_dpi    
-                                        from tb_familiar_responsable fr 
-                                        inner join tb_paciente p on p.Id_Familiar_Responsable = fr.id_familiar_responsable
-                                        inner join tb_municipio m on m.id_municipio = fr.id_municipio
-                                        inner join tb_departamento d on d.ID_DEPARTAMENTO = m.ID_DEPARTAMENTO
-                                        inner join tb_parentesco pr on pr.id_parentesco = fr.id_parentesco
-                                        where p.id_Paciente = ' . $id_Paciente);
+        $generalPacientes = DB::SELECT("select 
+                p.id_Paciente,
+                fr.id_familiar_responsable,
+                fr.nom_padre as nombrePadre,
+                fr.apell_padre as apellidoPadre,
 
+                ifnull(fr.estado_sit_dad, 2020) as estadoPadre,
+
+                fr.nom_madre as nombreMadre,
+                fr.apell_madre as apellidoMadre,
+
+                ifnull(fr.estado_sit_mom, 2020)as estadoMadre,
+
+                fr.nom_encar as nombreEncargado,
+                fr.apell_encar as apellidoEncargado,
+                fr.id_parentesco as idParentesco,
+                pr.tipo_parentesco as tipoParentesco,
+                fr.direccion,
+                fr.zona,
+                fr.colonia_barrio_aldea as coloniaBarrioAldea,
+                m.ID_MUNICIPIO,
+                m.MUNICIPIO,
+                d.ID_DEPARTAMENTO,
+                d.DEPARTAMENTO,
+                fr.Celular_1 as telefono_1,
+                IF(fr.Celular_2 = 0, '', fr.Celular_2 ) as telefono_2
+            from tb_familiar_responsable fr 
+            inner join tb_paciente p on p.Id_Familiar_Responsable = fr.id_familiar_responsable
+            inner join tb_municipio m on m.id_municipio = fr.id_municipio
+            inner join tb_departamento d on d.ID_DEPARTAMENTO = m.ID_DEPARTAMENTO
+            inner join tb_parentesco pr on pr.id_parentesco = fr.id_parentesco
+            where p.id_Paciente = " . $id_Paciente);
         foreach ($paciente as $pass) {
             $pacientes = $pass;
         }
